14b6c393acc79e15235a02837a8934c36d93df76
sanddep-dev <sandeep.n062000@gmail.com>
2023-05-14 19:43:35 +0530

    Intro Section Completed

--- src/Components/Intro/Intro.jsx
+++ src/Components/Intro/Intro.jsx
@@ -3,6 +3,13 @@ import './Intro.css'
 import Github from '../../img/github.png';
 import Linked from '../../img/linkedin.png';
 import Instagram from '../../img/instagram.png';
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import boy from '../../img/boy.png';
+import thumbup from '../../img/thumbup.png';
+import Crown from '../../img/crown.png';
+import glassesimoji from '../../img/glassesimoji.png';
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
 
 const Intro = () => {
   return (
@@ -21,7 +28,16 @@ const Intro = () => {
             </div>
         </div>
         <div className="i-right">
-            I am right
+            <img src={Vector1} alt="vector1" />
+            <img src={Vector2} alt="vector2" />
+            <img src={boy} alt="boy" />
+            <img src={glassesimoji} alt="glassesemoji" />
+            <div className='floatingdiv1'>
+                <FloatingDiv image={Crown} txt1='Web' txt2='Developer'/>
+            </div>
+            <div className='floatingdiv2'>
+                <FloatingDiv image={thumbup} txt1='Best Design' txt2='Award'/>
+            </div>
         </div>
     </div>
   )
