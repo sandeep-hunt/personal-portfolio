07e7a2344a3218cae28d26cb28cf2d060c0c63c1
sanddep-dev <sandeep.n062000@gmail.com>
2023-05-15 15:35:08 +0530

    Added Services Section

--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import './Services.css'
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import Card from '../Card/Card';
 
 const Services = () => {
   return (
     <div className="services">
-        services
+      <div className="awesome">
+        <span>My Awesome</span>
+        <span>services</span>
+        <span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</span>
+        <button className='button s-button'>Download CV</button>
+      </div>
+      <div className="cards">
+        <div className='card1'>
+          <Card
+          emoji = {HeartEmoji}
+          heading = {'Design'}
+          detail = {"Figma, Sketch, Photoshop, Adobe, Adobe xd"}
+          />
+        </div>
+        <div className='card2'>
+          <Card
+          emoji = {Glasses}
+          heading = {'Developer'}
+          detail = {"HTML, CSS, JavaScript, React"}
+          />
+        </div>
+        <div className='card3'>
+          <Card
+          emoji = {Humble}
+          heading = {'UI UX'}
+          detail = {"Figma, Sketch, Photoshop, Adobe, Adobe xd"}
+          />
+        </div>
+      </div>
     </div>
   )
 }
